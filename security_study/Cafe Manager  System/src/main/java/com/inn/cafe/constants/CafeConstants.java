83f2f62b6ca4049f020531edc18e63618c669cda
NghiaBH <znghiabhgmail.com>
2024-04-23 23:11:16 +0900

    23/4

---
+++ security_study/Cafe Manager  System/src/main/java/com/inn/cafe/constants/CafeConstants.java
@@ -0,0 +1,4 @@
+package com.inn.cafe.constants;
+
+public class CafeConstants {
+}
---
+++ security_study/Cafe Manager  System/src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.inn.cafe.serviceImpl;
+
+import com.inn.cafe.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UserServiceImpl implements UserService {
+}
